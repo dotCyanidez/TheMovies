ad9bb71fe002eb80825b43881bcb16fc8a1e80f5
Ulla Raaschou <ullaraaschou@gmail.com>
2023-10-09 11:45:43 +0200

    Nu med Cinema og CinemaRepo

---
+++ TheMovies/Model/CinemaRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheMovies.Model
+{
+    public class CinemaRepository
+    {
+        public Cinema Cinema { get; set; }
+
+        public List<Cinema> Cinemas { get; set; }
+
+        public CinemaRepository()
+        {
+          
+        }
+
+        public void Add(Cinema cinema) 
+        {
+            Cinemas.Add(cinema);
+        }
+
+    }
+
+    
+
+    
+
+}
