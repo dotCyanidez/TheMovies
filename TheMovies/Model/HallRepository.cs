89b2ffcaa6ab4f229ee94b8a35fd0feb64a8ad74
Abarbesgaard <Abarbesgaard@gmail.com>
2023-10-09 10:07:49 +0200

    Har implementeret Hall + HallRepo men sidst nævnte er ikke færdig

---
+++ TheMovies/Model/HallRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheMovies.Model
+{
+    public class HallRepository
+    {
+        public  Hall Hall { get; set; }
+        public List<Hall> Halls{ get; set; }
+
+        public void Add(Hall hall)
+        { Halls.Add(hall); }
+
+        public void GetAll()
+        {
+
+        }
+        public void Get(int Id)
+        {
+
+        }
+    }
+}
